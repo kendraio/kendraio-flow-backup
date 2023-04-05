825f1817a264f22c810147d9aeecc051b4f788c1
Luke Stanley <luke.stanley@gmail.com>
2023-04-05 09:56:53 +0100

    Save flows using single request instead of looping

--- download_flows_from_workflow_cloud.py
+++ download_flows_from_workflow_cloud.py
@@ -1,26 +1,27 @@
-import requests
-import json
 import os
+import json
+import requests
 
-API = 'https://app.kendra.io/api'
-flows = requests.get(API).json()
+try:
+    response = requests.get("https://app.kendra.io/api/")
+    flows = response.json()
 
-for flow in flows:
-    flow_name = flow['adapterName']
-    flow_id = flow['id']
-    print(f"{flow_name} {flow_id}")
+    for flow in flows:
+        flow_name = flow['adapterName']
+        flow_id = flow['workflowId']
+        print(f"{flow_name} {flow_id}")
 
-    try:
-        response = requests.get(f"{API}/{flow_name}/{flow_id}")
-        code = json.dumps(response.json(), indent=4)
+        try:
+            # Create a directory for the flow_name if it doesn't exist
+            if not os.path.exists(flow_name):
+                os.makedirs(flow_name)
 
-        # Create a directory for the flow_name if it doesn't exist
-        if not os.path.exists(flow_name):
-            os.makedirs(flow_name)
+            # Save the JSON file in the flow_name directory with flow_id as the filename
+            filename = f"{flow_name}/{flow_id}.json"
+            with open(filename, 'w') as file:
+                file.write(json.dumps(flow, indent=4))
+        except Exception as e:
+            print(f"Error downloading flow '{flow_name} {flow_id}': {e}")
 
-        # Save the JSON file in the flow_name directory with flow_id as the filename
-        filename = f"{flow_name}/{flow_id}.json"
-        with open(filename, 'w') as file:
-            file.write(code)
-    except json.decoder.JSONDecodeError as e:
-        print(f"Error parsing JSON for flow '{flow_name} {flow_id}': {e}")
+except Exception as e:
+    print(f"Error getting list of flows: {e}")
